609a5e75e086555ce3a27e87a624a2dd60037ae6
Alassane GUEYE <84050177+alassanegu@users.noreply.github.com>
2025-12-27 00:40:21 +0100

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,15 +7,15 @@ const Experience: React.FC = () => {
       title: "Ingénieur chef de projet développeur",
       company: "Désirade",
       location: "Lyon",
-      period: "Janvier 2025 – Avril 2025",
+      period: "Janvier 2025 – Aujourd'hui",
       description: "Direction de projets de développement, développement de solutions Power Platform, développement d'applications web.",
       technologies: ["Développement", "Coordination", "Power Platform", ".NET", "Lagoon", "SqlServer"]
     },
     {
-      title: "Solution Owner (Alternance)",
+      title: "Développeur Full Stack (Alternance)",
       company: "Oril Industrie",
       location: "Bolbec",
-      period: "Septembre 2023 – Septembre 2024",
+      period: "Septembre 2022 – Septembre 2024",
       description: "Développement de solutions Power Platform, analyse des besoins métier et création d'applications web.",
       technologies: ["Power Apps", "Power Automate", "Power BI", "SharePoint", "ASP.NET", ".NET", "WebForm", "SqlServer"]
     },
